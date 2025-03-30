0e030a0cde7e1ecc38dda76aa725f0e3bf3aaa39
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-03-31 04:12:48 +0530

    feat: edited preview

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Reachoout",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <main>{children}</main>
+        <Toaster />
+      </body>
     </html>
   );
 }
