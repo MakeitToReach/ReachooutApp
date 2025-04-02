7903630ea207b64a98302537967ab2ec3d7a0f2c
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-04-03 00:05:42 +0530

    devops: testing on dev branch

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,10 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// app.use(morgan(
-//     ':method :url :status :res[content-length] - :response-time ms'
-// ))
-
 // Test route
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server is running!");
