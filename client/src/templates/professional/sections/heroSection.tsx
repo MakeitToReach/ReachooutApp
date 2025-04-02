89f8aec91d565b977e3adf5c13b3ce148ee2793f
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-04-02 13:13:02 +0530

    feat: templates being fetched from the backend

--- client/src/templates/professional/sections/heroSection.tsx
+++ client/src/templates/professional/sections/heroSection.tsx
@@ -4,45 +4,41 @@ import Link from "next/link";
 import React from "react";
 
 interface PFHeroSetionProps {
-  title: string;
-  professions: string[];
-  btnText: string;
-  btnLink: string;
-  heroImgUrl: string;
+    title: string;
+    professions: string[];
+    btnText: string;
+    btnLink: string;
+    heroImgUrl: string;
 }
 export const PFHeroSection = ({
-  title,
-  professions,
-  btnLink,
-  btnText,
-  heroImgUrl,
+    title,
+    professions,
+    btnLink,
+    btnText,
+    heroImgUrl,
 }: PFHeroSetionProps) => {
-  return (
-    <section className="max-w-6xl mx-auto">
-      <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 px-4">
-        <div className="space-y-4">
-          {/* sm logo here */}
-          <div>
-            <h1
-              className="text-3xl md:text-6xl font-bold"
-            >
-              {title}
-            </h1>
-            <h2 className="text-3xl md:text-5xl font-bold text-green-600">
-              {professions[0]}
-            </h2>
-          </div>
-          <Link href={btnLink}>
-            <Button className="p-6 text-md rounded-sm">
-              {btnText}
-              <span>
-                <LucideArrowRight />
-              </span>
-            </Button>
-          </Link>
-        </div>
-        <img src={heroImgUrl} alt="heroimg" className="mt-20" />
-      </div>
-    </section>
-  );
+    return (
+        <section className="max-w-6xl mx-auto">
+            <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 px-4">
+                <div className="space-y-4">
+                    {/* sm logo here */}
+                    <div>
+                        <h1 className="text-3xl md:text-6xl font-bold">{title}</h1>
+                        <h2 className="text-3xl md:text-5xl font-bold text-green-600">
+                            {professions[0]}
+                        </h2>
+                    </div>
+                    <Link href={btnLink}>
+                        <Button className="p-6 text-md rounded-sm">
+                            {btnText}
+                            <span>
+                                <LucideArrowRight />
+                            </span>
+                        </Button>
+                    </Link>
+                </div>
+                <img src={heroImgUrl} alt="heroimg" className="mt-20" />
+            </div>
+        </section>
+    );
 };
