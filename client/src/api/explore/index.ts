2d5753191474e387a0faf3eeaf71f17f41509ac5
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-04-03 14:49:34 +0530

    build: fixed build errors

--- client/src/api/explore/index.ts
+++ client/src/api/explore/index.ts
@@ -3,12 +3,13 @@ import { api } from "../axios.config";
 import { getToken } from "@/lib/isAuthenticated";
 
 export const fetchAllTemplates = async () => {
-  const token = getToken();
-  const response = await api.get("/v1/template/all", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const token = getToken();
+    const response = await api.get("/v1/template/all", {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+    });
 
-  return response.data;
+    return response.data;
 };
