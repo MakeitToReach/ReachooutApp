9bd1573b93dfe0ca88ce5c81191728b1fe1bff5e
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-04-25 15:40:26 +0530

    fix: fixed editor panel

--- client/src/app/editor/[slug]/page.tsx
+++ client/src/app/editor/[slug]/page.tsx
@@ -10,72 +10,82 @@ import { Button } from "@/components/ui/button";
 import { LucideSidebarOpen } from "lucide-react";
 
 const EditorPage = () => {
-  const { slug } = useParams();
-  const searchParams = useSearchParams();
-  const [editorOpen, setEditorOpen] = useState(true);
+    const { slug } = useParams();
+    const searchParams = useSearchParams();
+    const [editorOpen, setEditorOpen] = useState(true);
 
-  const isNew = searchParams.has("new");
-  const { resetData } = usePortfolioStore();
-  const isEditing = searchParams.has("edit");
+    const isNew = searchParams.has("new");
+    const { resetData } = usePortfolioStore();
+    const isEditing = searchParams.has("edit");
 
-  const templateKey = slug as keyof typeof TEMPLATE_REGISTRY;
-  const template = TEMPLATE_REGISTRY[templateKey];
+    const templateKey = slug as keyof typeof TEMPLATE_REGISTRY;
+    const template = TEMPLATE_REGISTRY[templateKey];
 
-  useEffect(() => {
-    const data = usePortfolioStore.getState().data;
-    console.log("current data in store for editing:", data);
-    if (template && isNew) {
-      resetData(template.data); // Load default content
-    }
+    useEffect(() => {
+        const data = usePortfolioStore.getState().data;
+        console.log("current data in store for editing:", data);
+        if (template && isNew) {
+            resetData(template.data); // Load default content
+        }
 
-    return () => {
-      if (isNew) {
-        resetData(null);
-      }
+        return () => {
+            if (isNew) {
+                resetData(null);
+            }
+        };
+    }, [slug, isNew]);
+
+    const toggleEditor = () => {
+        setEditorOpen((prev) => !prev);
     };
-  }, [slug, isNew]);
 
-  const toggleEditor = () => {
-    setEditorOpen((prev) => !prev);
-  };
+    if (!template) return <p>Template not found</p>;
 
-  if (!template) return <p>Template not found</p>;
+    return (
+        <div className="relative w-full flex overflow-x-hidden">
+            {/* Editor Panel */}
+            {editorOpen ? (
+                <>
+                    <div className="w-full md:w-[30%]" />
+                    <div
+                        className={cn("w-full md:w-[30%] fixed top-0 left-0 z-[100]", {
+                            hidden: !editorOpen,
+                        })}
+                    >
+                        {isEditing ? (
+                            <EditorPanel
+                                toggleEditor={toggleEditor}
+                                isEditing
+                                templateSchema={template.editorSchema}
+                            />
+                        ) : (
+                            <EditorPanel
+                                templateSchema={template.editorSchema}
+                                toggleEditor={toggleEditor}
+                            />
+                        )}
+                    </div>
+                </>
+            ) : (
+                <Button
+                    variant={"default"}
+                    className="fixed bottom-4 left-4 z-[100]"
+                    onClick={toggleEditor}
+                >
+                    <LucideSidebarOpen className="w-6 h-6" />
+                </Button>
+            )}
 
-  return (
-    <div className="relative w-full flex overflow-x-hidden">
-      {/* Editor Panel */}
-      {editorOpen ? (
-        <div className={cn("w-full md:w-[30%]", { hidden: !editorOpen })}>
-          {isEditing ? (
-            <EditorPanel
-              toggleEditor={toggleEditor}
-              isEditing
-              templateSchema={template.editorSchema}
-            />
-          ) : (
-            <EditorPanel
-              templateSchema={template.editorSchema}
-              toggleEditor={toggleEditor}
-            />
-          )}
+            {/* Live Preview */}
+            <div
+                className={cn("md:w-[70%] hidden md:block flex-grow", {
+                    "w-full block": !editorOpen,
+                })}
+            >
+                <LivePreview templateComponent={template.component} />
+            </div>
         </div>
-      ) : (
-          <Button variant={"default"} className="fixed bottom-4 left-4 z-[100]" onClick={toggleEditor}>
-            <LucideSidebarOpen className="w-6 h-6" />
-          </Button>
-
-      )}
-
-      {/* Live Preview */}
-      <div
-        className={cn("md:w-[70%] hidden md:block flex-grow", {
-          "w-full block": !editorOpen,
-        })}
-      >
-        <LivePreview templateComponent={template.component} />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default EditorPage;
