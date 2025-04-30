e5efd3cb0a4f986999a39d1971df486d8157e18d
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-04-30 20:52:15 +0530

    fix: changed user preview to reflect latest changes

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -7,31 +7,35 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const UserPreview = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const [data, setData] = useState<GenericTemplateSchema | null>(null);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [data, setData] = useState<GenericTemplateSchema | null>(null);
 
-    const templateKey = searchParams.get(
-        "template",
-    ) as keyof typeof TEMPLATE_REGISTRY;
-    const template = TEMPLATE_REGISTRY[templateKey];
+  const templateKey = searchParams.get(
+    "template",
+  ) as keyof typeof TEMPLATE_REGISTRY;
+  const template = TEMPLATE_REGISTRY[templateKey];
 
-    useEffect(() => {
-        if (!template) return; // Prevents fetching if template is invalid
+  useEffect(() => {
+    if (!template) return; // Prevents fetching if template is invalid
 
-        const fetchData = async () => {
-            const fetchedData = await getUserTemplateData(templateKey);
-            setData(fetchedData.userTemplateData);
-            if (!fetchedData) router.push(`/`);
-        };
+    const fetchData = async () => {
+      const fetchedData = await getUserTemplateData(templateKey);
+      setData(fetchedData.userTemplateData);
+      if (!fetchedData) router.push(`/`);
+    };
 
-        fetchData();
-    }, [templateKey]);
+    fetchData();
+  }, [templateKey]);
 
-    if (!template) return <p>Template not found</p>;
-    if (!data) return <p>Loading...</p>;
+  if (!template) return <p>Template not found</p>;
+  if (!data) return <p>Loading...</p>;
 
-    return <template.component data={data} />;
+  return (
+    <div style={(data.theme as Record<string, string>) || {}}>
+      <template.component data={data} />
+    </div>
+  );
 };
 
 export default UserPreview;
