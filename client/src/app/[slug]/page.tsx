501d06301f7f53d55fe363cb87391a5cb2df0b8c
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-05-06 21:36:54 +0530

    fix-test: user final site theme issue

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -1,5 +1,7 @@
+export const dynamic = "force-dynamic"
 import { UserPreview } from "@/components/editor-components/userPreview";
 import React from "react";
+
 
 const page = () => {
     return <UserPreview />;
