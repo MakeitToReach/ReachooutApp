aef741bc94e08952411bd5b95b2785f1098c6a14
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-05-09 23:53:51 +0530

    fix-test: user final site theme issue

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -1,7 +1,15 @@
-export const dynamic = "force-dynamic"
-import { UserPreview } from "@/components/editor-components/userPreview";
+// export const dynamic = "force-dynamic";
+// import { UserPreview } from "@/components/editor-components/userPreview";
+import dynamic from "next/dynamic";
 import React from "react";
 
+// somewhere like pages/preview.tsx or a parent component
+const UserPreview = dynamic(
+    () => import("@/components/editor-components/userPreview"),
+    {
+        ssr: false,
+    },
+);
 
 const page = () => {
     return <UserPreview />;
