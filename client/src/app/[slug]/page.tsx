b6d6f76eeabe8c2bc5ae22e636c6fd904881c9ee
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-05-10 00:57:17 +0530

    build: fixed build errors

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -1,15 +1,6 @@
-// export const dynamic = "force-dynamic";
-// import { UserPreview } from "@/components/editor-components/userPreview";
-import dynamic from "next/dynamic";
+import UserPreview from "@/components/editor-components/userPreview";
 import React from "react";
 
-// somewhere like pages/preview.tsx or a parent component
-const UserPreview = dynamic(
-    () => import("@/components/editor-components/userPreview"),
-    {
-        ssr: false,
-    },
-);
 
 const page = () => {
     return <UserPreview />;
