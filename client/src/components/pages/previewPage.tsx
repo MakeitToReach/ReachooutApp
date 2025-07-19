a8a93eb930306dc2fb0766fc8030da87ea046b03
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-07-19 18:51:34 +0530

    feat: integrated previews to have categorical data

--- client/src/components/pages/previewPage.tsx
+++ client/src/components/pages/previewPage.tsx
@@ -4,45 +4,49 @@ import { TEMPLATE_REGISTRY } from "@/lib/templateRegistry";
 import { usePortfolioStore } from "@/store/portfolio.store";
 import { useParams, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { Loading } from "../editor-components/loading";
 
 const PreviewPage = () => {
-    const params = useParams<{ slug: string }>();
-    const slug = params?.slug;
+  const params = useParams<{ slug: string }>();
+  const slug = params?.slug;
 
-    const searchParams = useSearchParams();
-    const { data, resetData } = usePortfolioStore();
+  // const searchParams = useSearchParams();
+  const { data } = usePortfolioStore();
 
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-    const templateKey = slug as keyof typeof TEMPLATE_REGISTRY;
-    const SelectedTemplate = TEMPLATE_REGISTRY[templateKey];
-    const isNew = searchParams?.has("new");
+  const templateKey = slug as keyof typeof TEMPLATE_REGISTRY;
+  const SelectedTemplate = TEMPLATE_REGISTRY[templateKey];
+  // const isNew = searchParams?.has("new");
 
-    useEffect(() => {
-        if (SelectedTemplate && isNew) {
-            resetData(SelectedTemplate.data);
-            setLoading(false);
-        }
-    }, []);
+  // useEffect(() => {
+  //     if (SelectedTemplate && isNew) {
+  //         resetData(SelectedTemplate.data);
+  //         setLoading(false);
+  //     }
+  // }, []);
 
-    if (!slug || typeof slug != "string" || loading) {
-        return <div>Loading</div>;
-    }
-    if (!SelectedTemplate) {
-        return <div>Template not found</div>;
-    }
+  if (!slug || typeof slug != "string" || loading) {
+    return <Loading />;
+  }
+  if (!data) {
+    return <Loading />;
+  }
+  if (!SelectedTemplate) {
+    return <div>Template not found</div>;
+  }
 
-    return (
-        <div
-            className="theme-wrapper relative"
-            style={(data?.theme as Record<string, string>) || {}}
-        >
-            <div className="fixed bottom-4 right-0 -translate-x-1/2 md:translate-x-0 md:bottom-4 md:left-4 w-[200px] z-50">
-                <ThemeSelectDropdown />
-            </div>
-            <SelectedTemplate.component data={data!} />
-        </div>
-    );
+  return (
+    <div
+      className="theme-wrapper relative"
+      style={(data?.theme as Record<string, string>) || {}}
+    >
+      <div className="fixed bottom-4 right-0 -translate-x-1/2 md:translate-x-0 md:bottom-4 md:left-4 w-[200px] z-50">
+        <ThemeSelectDropdown />
+      </div>
+      <SelectedTemplate.component data={data} />
+    </div>
+  );
 };
 
 export default PreviewPage;
