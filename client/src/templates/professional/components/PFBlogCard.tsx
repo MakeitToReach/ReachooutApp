e280970cfa612e120937accad0822ce4e821f0c5
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-07-31 15:54:54 +0530

    test: testing line clamp for blog card

--- client/src/templates/professional/components/PFBlogCard.tsx
+++ client/src/templates/professional/components/PFBlogCard.tsx
@@ -3,30 +3,38 @@ import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
 
 interface PFBlogCardProps {
-    imgUrl: string;
-    title: string;
-    description: string;
-    btnText?: string;
-    btnLink?: string;
+  imgUrl: string;
+  title: string;
+  description: string;
+  btnText?: string;
+  btnLink?: string;
 }
-export const PFBlogCard = ({ imgUrl, title, description, btnText, btnLink }: PFBlogCardProps) => {
-    const blogContent = { imgUrl, title, description, btnText, btnLink };
-    return (
-        <div className="h-fit w-full bg-template-primary rounded-sm overflow-hidden space-y-6 pb-6 text-template-text-primary">
-            <Image
-                quality={100}
-                src={imgUrl || "/placeholder.png"}
-                alt="blog-img"
-                width={400}
-                height={240}
-                className="w-full h-[15rem] rounded-sm object-cover"
-            />
-            <div className="space-y-2 text-left">
-                <h2 className="font-semibold sm:text-lg text-2xl tracking-tight line-clamp-2">{title}</h2>
+export const PFBlogCard = ({
+  imgUrl,
+  title,
+  description,
+  btnText,
+  btnLink,
+}: PFBlogCardProps) => {
+  const blogContent = { imgUrl, title, description, btnText, btnLink };
+  return (
+    <div className="h-fit w-full bg-template-primary rounded-sm overflow-hidden space-y-6 pb-6 text-template-text-primary">
+      <Image
+        quality={100}
+        src={imgUrl || "/placeholder.png"}
+        alt="blog-img"
+        width={400}
+        height={240}
+        className="w-full h-[15rem] rounded-sm object-cover"
+      />
+      <div className="space-y-2 text-left">
+        <h2 className="font-semibold sm:text-lg text-2xl tracking-tight line-clamp-2">
+          {title}
+        </h2>
 
-                <div
-                    className="
-    prose prose-xl sm:prose-base max-w-none text-template-text-primary prose-p:line-clamp-2
+        <div
+          className="
+    prose prose-xl sm:prose-base max-w-none text-template-text-primary 
     prose-p:text-template-text-primary
     prose-strong:text-template-text-primary
     prose-h1:text-template-text-primary
@@ -35,14 +43,20 @@ export const PFBlogCard = ({ imgUrl, title, description, btnText, btnLink }: PFB
     prose-h4:text-template-text-primary
     prose-h5:text-template-text-primary
     prose-h6:text-template-text-primary
-    line-clamp-2
+    line-clamp-3
   "
-                    dangerouslySetInnerHTML={{ __html: description }}
-                />
-                <ViewMoreDrawer type="Blog" content={blogContent}>
-                    <button className="hover:underline text-xl sm:text-base flex items-center gap-1">Read more <ArrowUpRight size={16} className="text-template-text-accent-tertiary" /></button>
-                </ViewMoreDrawer>
-            </div>
-        </div>
-    );
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+        <ViewMoreDrawer type="Blog" content={blogContent}>
+          <button className="hover:underline text-xl sm:text-base flex items-center gap-1">
+            Read more{" "}
+            <ArrowUpRight
+              size={16}
+              className="text-template-text-accent-tertiary"
+            />
+          </button>
+        </ViewMoreDrawer>
+      </div>
+    </div>
+  );
 };
