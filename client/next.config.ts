3760de3c5405b231dcf85c6906eff843c1d649fb
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-08-23 19:33:55 +0530

    feat: added policy pages

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,28 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: false,
+  reactStrictMode: false,
   /* config options here */
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '*',
-            },
-        ]
-
-    },
-    // Allow cross-origin requests from subdomains in development
-    allowedDevOrigins: [
-        'localhost',
-        '*.localhost',
-        '*.localhost:3000',
-        '*.reachoout.com',
-        '*-*.reachoout.com',
-        // Allow any subdomain pattern for localhost
-        '*-*.localhost',
-        '*-*.localhost:3000',
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "https",
+        hostname: "reachooutassets.s3.amazonaws.com",
+      },
     ],
+  },
+  // Allow cross-origin requests from subdomains in development
+  allowedDevOrigins: [
+    "localhost",
+    "*.localhost",
+    "*.localhost:3000",
+    "*.reachoout.com",
+    "*-*.reachoout.com",
+    // Allow any subdomain pattern for localhost
+    "*-*.localhost",
+    "*-*.localhost:3000",
+  ],
 };
 
 export default nextConfig;
